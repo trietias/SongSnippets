21b8124a0b76215d32e5f66863dd73c33c1514fb
Triet Khuc <triet.khuc@gmail.com>
2016-12-22 21:17:16 -0500

    adding project

---
+++ app/src/main/java/com/tmk/songsnippets/DBHandler.java
@@ -0,0 +1,265 @@
+package com.tmk.songsnippets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by triet on 12/20/2016.
+ */
+public class DBHandler extends SQLiteOpenHelper {
+
+    //The Android's default system path of your application database.
+    private static String DB_PATH = "/data/data/com.tmk.songsnippets/databases/";
+
+    private static String DB_NAME = "songDB.sqlite";
+
+    private SQLiteDatabase myDataBase;
+
+    private final Context myContext;
+
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context
+     */
+    public DBHandler(Context context) {
+
+        super(context, DB_NAME, null, 1);
+        this.myContext = context;
+    }
+
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDataBase() throws IOException {
+
+        boolean dbExist = checkDataBase();
+
+        if(dbExist){
+            //do nothing - database already exist
+        }else{
+
+            //By calling this method and empty database will be created into the default system path
+            //of your application so we are gonna be able to overwrite that database with our database.
+            this.getReadableDatabase();
+
+            try {
+
+                copyDataBase();
+
+            } catch (IOException e) {
+
+                throw new Error("Error copying database");
+
+            }
+        }
+
+    }
+
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase(){
+
+        SQLiteDatabase checkDB = null;
+
+        try{
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+        }catch(SQLiteException e){
+
+            //database does't exist yet.
+
+        }
+
+        if(checkDB != null){
+
+            checkDB.close();
+
+        }
+
+        return checkDB != null ? true : false;
+    }
+
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transfering bytestream.
+     * */
+    private void copyDataBase() throws IOException{
+
+        //Open your local db as the input stream
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+
+        // Path to the just created empty db
+        String outFileName = DB_PATH + DB_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+    }
+
+    public void openDataBase() throws SQLException {
+
+        //Open the database
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+    }
+
+    @Override
+    public synchronized void close() {
+
+        if(myDataBase != null)
+            myDataBase.close();
+
+        super.close();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    // Add your public helper methods to access and get content from the database.
+    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
+    // to you to create adapters for your views.
+
+    public List<String> getAllData(String text) {
+        List<String> dataList = new ArrayList<String>();
+        String selectQuery = "SELECT " + text + " FROM " + "Songs";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                // Adding the text to list
+                dataList.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        // return data list
+        return dataList;
+    }
+
+    public List<String> getCategoryData(String category) {
+        List<String> dataList = new ArrayList<String>();
+        String selectQuery = "SELECT SongTitle FROM Songs WHERE Category1 = '" + category +
+                "' OR Category2 = '" + category + "' OR Category3 = '" + category + "' OR Category4 = '"
+                + category + "' OR Category5 = '" + category + "' OR Category6 = '" + category +
+                "' OR Category7 = '" + category + "' OR Category8 = '" + category +
+                "' OR Category9 = '" + category + "' OR Category10 = '" + category +"'";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                // Adding the text to list
+                dataList.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        // return data list
+        return dataList;
+    }
+
+    public String getResourceValue(String title) {
+        String selectQuery = "SELECT MAX(ResourceValue) FROM Songs WHERE SongTitle = '" + title + "'";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        String resource = "";
+        if (cursor.moveToFirst()) {
+            resource = cursor.getString(0);
+            cursor.close();
+            return resource;
+        } else {
+            return resource;
+        }
+    }
+
+    public String getArtist(String title) {
+        String selectQuery = "SELECT MAX(SongArtist) FROM Songs WHERE SongTitle = '" + title + "'";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        String resource = "";
+        if (cursor.moveToFirst()) {
+            resource = cursor.getString(0);
+            cursor.close();
+            return resource;
+        } else {
+            return resource;
+        }
+    }
+
+    public String getCategory(String title) {
+        String selectQuery = "SELECT MAX(Category2) FROM Songs WHERE SongTitle = '" + title + "'";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        String resource = "";
+        if (cursor.moveToFirst()) {
+            resource = cursor.getString(0);
+            cursor.close();
+            return resource;
+        } else {
+            return resource;
+        }
+    }
+
+    public List<String> getCategories() {
+        List<String> dataList = new ArrayList<String>();
+        String selectQuery = "SELECT Distinct Category1 FROM Songs WHERE Category1 IS NOT NULL " +
+                "UNION SELECT Distinct Category2 FROM Songs WHERE Category2 IS NOT NULL " +
+                "UNION SELECT Distinct Category3 FROM Songs WHERE Category3 IS NOT NULL " +
+                "UNION SELECT Distinct Category4 FROM Songs WHERE Category4 IS NOT NULL ";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                // Adding the text to list
+                dataList.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        // return data list
+        return dataList;
+    }
+
+}
---
+++ app/src/main/java/com/tmk/songsnippets/MainActivity.java
@@ -0,0 +1,236 @@
+package com.tmk.songsnippets;
+
+import android.database.SQLException;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer songController;
+    final private Integer[] intervals = new Integer[]{1,2,3,5,10,15};
+    final private Integer[] points = new Integer[]{1000,800,600,400,200,100};
+    private int intervalTracker = 0;
+    private List<String> categoryList;
+    private List<String> songList;
+    private Spinner categorySpinner;
+    private Spinner songSpinner;
+    Integer team1 = 0;
+    Integer team2 = 0;
+    TextView intervalTextView;
+    TextView songTitleTextView;
+    TextView songArtistTextView;
+    TextView songCategoryTextView;
+    TextView team1Score;
+    TextView team2Score;
+    String songChoice = "";
+    String songTitle;
+    String songArtist;
+    int songResource = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intervalTextView = (TextView) findViewById(R.id.intervalText);
+        songTitleTextView = (TextView) findViewById(R.id.songTitle);
+        songArtistTextView = (TextView) findViewById(R.id.artistName);
+        songCategoryTextView = (TextView) findViewById(R.id.categoryName);
+        team1Score = (TextView) findViewById(R.id.team1Points);
+        team1Score.setText("0");
+        team2Score = (TextView) findViewById(R.id.team2Points);
+        team2Score.setText("0");
+        initialSetUp();
+
+        categorySpinner = (Spinner)findViewById(R.id.categorySpinner);
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categoryList);
+        categorySpinner.setAdapter(categoryAdapter);
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                DBHandler handler = new DBHandler(getBaseContext());
+                songList = handler.getCategoryData(categoryList.get(position));
+                handler.close();
+                ArrayAdapter<String> songAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_dropdown_item, songList);
+                songSpinner.setAdapter(songAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        songSpinner = (Spinner)findViewById(R.id.songSpinner);
+        ArrayAdapter<String> songAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, songList);
+        songSpinner.setAdapter(songAdapter);
+        songSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                setUp();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        setUp();
+    }
+
+    public void play(View view) {
+        if(songController != null) {
+            if (songController.isPlaying()) {
+                try {
+                    songController.stop();
+                    songController.release();
+                    songController = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        songController = MediaPlayer.create(this,songResource);
+        int playTime = intervals[intervalTracker];
+        songController.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                try {
+                    songController.stop();
+                    songController.release();
+                    songController = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        songController.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (songController != null) {
+                        if (songController.isPlaying()) {
+                            songController.stop();
+                            songController.release();
+                            songController = null;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, playTime * 1000);
+    }
+
+    public void editInterval(View view) {
+        if (view == findViewById(R.id.forwardButton)) {
+            if (intervalTracker < 5) {
+                intervalTracker++;
+            }
+        } else {
+            if (intervalTracker > 0) {
+                intervalTracker--;
+            }
+        }
+        String s = Integer.toString(intervals[intervalTracker]);
+        intervalTextView.setText(s);
+    }
+
+    public void initialSetUp() {
+        DBHandler handler = new DBHandler(this);
+
+        try {
+
+            handler.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            handler.openDataBase();
+
+        }catch(SQLException sqle){
+
+            throw sqle;
+
+        }
+
+        categoryList = handler.getCategories();
+        songList = handler.getAllData("SongTitle");
+        handler.close();
+    }
+
+    public void setUp() {
+        intervalTracker = 0;
+        String s = Integer.toString(intervals[intervalTracker]);
+        intervalTextView.setText(s);
+        String artist = "By: ";
+        String category = "Category: ";
+        DBHandler handler = new DBHandler(this);
+        songTitle = songSpinner.getSelectedItem().toString();
+        songChoice = handler.getResourceValue(songTitle);
+        songResource = getResourceId(songChoice, "raw", getPackageName());
+        songTitleTextView.setText(songTitle);
+        artist += handler.getArtist(songTitle);
+        songArtistTextView.setText(artist);
+        category += handler.getCategory(songTitle);
+        songCategoryTextView.setText(category);
+        handler.close();
+    }
+
+    public int getResourceId(String pVariableName, String pResourcename, String pPackageName)
+    {
+        try {
+            return getResources().getIdentifier(pVariableName, pResourcename, pPackageName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public void random(View view) {
+        int rand = (int) Math.floor(Math.random()*songList.size());
+        songSpinner.setSelection(rand);
+        intervalTracker = 0;
+        setUp();
+    }
+
+    public void increaseScore(View view) {
+        int pointValue = points[intervalTracker];
+        if(view == findViewById(R.id.team1Add)) {
+            team1 += pointValue;
+        } else {
+            team2 += pointValue;
+        }
+        String t1points = team1.toString();
+        String t2points = team2.toString();
+        team1Score.setText(t1points);
+        team2Score.setText(t2points);
+    }
+
+    public void resetScore(View view) {
+        team1 = 0;
+        team2 = 0;
+        String t1points = team1.toString();
+        String t2points = team2.toString();
+        team1Score.setText(t1points);
+        team2Score.setText(t2points);
+    }
+
+}
